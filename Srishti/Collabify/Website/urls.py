251dda830d35fda19c86cd31d21417107372b332
VergilX <abhinand.dmanoj@gmail.com>
2022-06-06 23:00:01 +0530

    Added some views.py logic

--- Srishti/Collabify/Website/urls.py
+++ Srishti/Collabify/Website/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login", views.login_home, name="login"),
+    path("logout", views.logout_home, name="logout"),
+    path("register", views.register, name="register"), 
+    path("profile", views.userpage, name="user")
 ]
