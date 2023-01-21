daae76883f7a70f94aa9957181ae3355476ec620
VergilX <abhinand.dmanoj@gmail.com>
2023-01-21 19:40:31 +0530

    API works!

--- EveHack/GarduinoV2/main/models.py
+++ EveHack/GarduinoV2/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class PlantData(models.Model):
+
+    # Different model statuses of plant
+    PLANT_STATUS = [
+        ("H", "Healthy"),
+        ("I", "Infected"),
+        ("W", "Withered"),
+        ("N", "No stats available"),
+    ]
+
+    # All required data from plant
+    name = models.CharField(max_length=50, default="Not set")
+    temp = models.IntegerField(default=0)
+    humidity = models.IntegerField(default=0)
+    next_spray = models.TimeField()
+    fertilizer_level = models.IntegerField(default=0)
+    led_intensity = models.IntegerField(default=0)
+    time_to_sundown = models.TimeField()
+    status = models.CharField(max_length=1, choices=PLANT_STATUS, default="N")
+
+    def __str__(self):
+        return f"Plant: {self.name}"
--- EveHack/GarduinoV2/main/urls.py
+++ EveHack/GarduinoV2/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path("api/", views.get_alldata, name="modifyall"),
+    path("api/<int:pk>/", views.modify_singleplant, name="modify_single"),
 ]
